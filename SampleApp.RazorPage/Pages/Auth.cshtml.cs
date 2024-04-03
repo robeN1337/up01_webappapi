5c9a9c997617aeb1b92bae6ea7f88e5a1ef317a2
robbz <17ip214@git.scc>
2024-04-03 12:14:49 +0300

    red markup for admin name

--- SampleApp.RazorPage/Pages/Auth.cshtml.cs
+++ SampleApp.RazorPage/Pages/Auth.cshtml.cs
@@ -31,24 +31,36 @@ public class AuthModel : PageModel
         var response = await _http.GetAsync($"{_http.BaseAddress}/users/auth?email={user.Email}&password={user.Password}");
        
         var current_user = await response.Content.ReadFromJsonAsync<User>();
+        try
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                HttpContext.Session.SetString("SampleSession", $"{current_user.Id}");
+                _f.Flash(Types.Success, $"Добро пожаловать, {current_user.Name}!");
+                return RedirectToPage("Index");
+            }
+            else
+            {                
+                _f.Flash(Types.Danger, $"Неверный логин или пароль!");
+                _log.LogError($"Статус код: {response.StatusCode}");
+                return Page();
+            }
+        }
+        catch (Exception ex)
+        {
+            _log.LogError($"Ошибка: {ex.InnerException.Message}");
+            _f.Flash(Types.Danger, $"Ошибка авторизации: {ex.InnerException.Message}", dismissable: false);
+            return RedirectToPage("Auth");
 
-        if (response.IsSuccessStatusCode)
-        {
-            HttpContext.Session.SetString("SampleSession", $"{current_user.Id}");
-            _f.Flash(Types.Success, $"Добро пожаловать, {current_user.Name}!");
-            return RedirectToPage("Index");
         }
-        else
-        {
-            _f.Flash(Types.Danger, $"Неверный логин или пароль!");
-            return Page();
-        }
+        
     }
 
     public IActionResult OnGetLogout()
     {
         // сброс сессии
         HttpContext.Session.Clear();
+        _f.Flash(Types.Success, $"Данные очищены!", dismissable: false);
         return RedirectToPage("Auth");
     }
 
